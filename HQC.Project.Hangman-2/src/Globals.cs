b3047343dda3cfca3c1810284fa22e1f9eae11d9
zmihaylov <zlatimirmihailov@gmail.com>
2015-07-16 10:36:29 +0300

    Change a constant

--- HQC.Project.Hangman-2/src/Globals.cs
+++ HQC.Project.Hangman-2/src/Globals.cs
@@ -3,6 +3,6 @@
     public static class Globals
     {
         public const int ScoreBoardSize = 5;
-        public const int IndicationThatScoreBoardIsFull = -1;
+        public const int LastPositionInScoreBoard = 4;
     }
 }
