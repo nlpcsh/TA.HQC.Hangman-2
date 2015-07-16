4c82b5a5c532ff46088fe96c0079c42c82b6cc59
zmihaylov <zlatimirmihailov@gmail.com>
2015-07-16 17:26:06 +0300

    Update Globals.cs

--- HQC.Project.Hangman-2/src/Globals.cs
+++ HQC.Project.Hangman-2/src/Globals.cs
@@ -3,7 +3,7 @@
     public static class Globals
     {
         public const int ScoreBoardSize = 5;
-        public const int LastPositionInScoreBoard = -1;
+        public const int LastPositionInScoreBoard = 4;
         public const string FreePositionInScoreBoars = "unknown-0";
         public const string NoPlayer = "No Player";
     }
