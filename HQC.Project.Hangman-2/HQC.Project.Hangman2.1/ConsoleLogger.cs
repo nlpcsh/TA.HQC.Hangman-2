78d7ab125151c16e4d5e1ef36ef458ea2b3be3d1
Nikolay Petrov <nikolay.petrov@abv.bg>
2015-08-10 11:04:52 +0300

    implementation of "Dependency Inversion" for printing the output on the Console

---
+++ HQC.Project.Hangman-2/HQC.Project.Hangman2.1/ConsoleLogger.cs
@@ -0,0 +1,13 @@
+﻿namespace HQC.Project.Hangman2._1
+{
+    using HQC.Project.Hangman2._1.interfaces;
+    //using System;
+
+    public class ConsoleLogger : ILogger
+    {
+        public void LogLine(string line)
+        {
+            System.Console.WriteLine(line);
+        }
+    }
+}
---
+++ HQC.Project.Hangman-2/HQC.Project.Hangman2.1/interfaces/ILogger.cs
@@ -0,0 +1,7 @@
+﻿namespace HQC.Project.Hangman2._1.interfaces
+{
+    internal interface ILogger
+    {
+        void LogLine(string logger);
+    }
+}
