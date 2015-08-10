f675cb943008be3cba7df65368c06581cb2aeda0
Nikolay Petrov <nikolay.petrov@abv.bg>
2015-08-10 10:32:07 +0300

    added Hangman class

---
+++ HQC.Project.Hangman-2/HQC.Project.Hangman2.1/Hangman.cs
@@ -0,0 +1,28 @@
+﻿namespace HQC.Project.Hangman2._1
+{
+    //using System;
+    //using System.Collections.Generic;
+    //using System.Linq;
+    //using System.Text;
+    //using System.Threading.Tasks;
+
+    public class Hangman
+    {
+        private static bool gameIsOn = true;
+
+        static void Main(string[] args)
+        {
+            var commantExecutor = new CommandExecuter();
+            var scoreBoard = new ScoreBoard();
+            var wordSelector = new WordSelector();
+
+            while (gameIsOn)
+            {
+                var player = new Player();
+
+
+                gameIsOn = false;
+            }
+        }
+    }
+}
