a530a4bb9d0e679bddf9c5b53adceb1b8ad954f1
EmilMitev <Mitev.e93@gmail.com>
2015-09-05 19:52:56 +0300

    Fixed more StyleCop warnings

--- HQC.Project.Hangman-2/HQC.Project.Hangman2.1/ConsoleLogger.cs
+++ HQC.Project.Hangman-2/HQC.Project.Hangman2.1/ConsoleLogger.cs
@@ -1,8 +1,8 @@
 ﻿namespace HQC.Project.Hangman2._1
 {
     using HQC.Project.Hangman2._1.Interfaces;
-    //using System;
 
+    // using System;
     public class ConsoleLogger : ILogger
     {
         public void LogLine(string line)
