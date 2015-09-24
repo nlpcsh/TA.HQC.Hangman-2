e7ca4e3918de13f5c2339f68039b8643c047cd7d
ReniGetskova <reni.getskova@gmail.com>
2015-09-24 19:18:41 +0300

    Added IImporter.cs and IPlayer.cs interfaces

---
+++ HQC.Project.Hangman-2/HQC.Project.Hangman2.1/interfaces/IImporter.cs
@@ -0,0 +1,9 @@
+﻿namespace HQC.Project.Hangman2._1.Interfaces
+{
+    using System.Collections.Generic;
+
+    public interface IImporter
+    {
+        IList<IPlayer> Import();
+    }
+}
---
+++ HQC.Project.Hangman-2/HQC.Project.Hangman2.1/interfaces/IPlayer.cs
@@ -0,0 +1,13 @@
+﻿namespace HQC.Project.Hangman2._1.Interfaces
+{
+    public interface IPlayer
+    {
+        string Name { get; set; }
+
+        int Mistakes { get; set; }
+
+        int Lives { get; set; }
+
+        int Compare(IPlayer otherPlayer);
+    }
+}
