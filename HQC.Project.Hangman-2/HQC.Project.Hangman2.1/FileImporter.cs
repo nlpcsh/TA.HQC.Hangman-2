ca3434646de5be704bd3fe7a2cc5d82b8c0a4cc9
ReniGetskova <reni.getskova@gmail.com>
2015-09-25 11:06:52 +0300

    Added FileImporter.cs file

---
+++ HQC.Project.Hangman-2/HQC.Project.Hangman2.1/FileImporter.cs
@@ -0,0 +1,17 @@
+﻿using HQC.Project.Hangman2._1.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HQC.Project.Hangman2._1
+{
+    public class FileImporter : IImporter
+    {
+        public IList<IPlayer> Import()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
