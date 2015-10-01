4bb5092f0ccb8658067a18ede24aff78027aa76e
danielkaradaliev <daniel_karadaliev@abv.bg>
2015-10-02 00:12:18 +0300

    Added class ContentFolderReader (in Importers), which (should) returns the names of the Categories; Added new Constant: CategoriesPath

---
+++ HQC.Project.Hangman-2/HQC.Project.Hangman2.1/Importers/FolderContentReader.cs
@@ -0,0 +1,23 @@
+﻿namespace HQC.Project.Hangman2.Importers
+{
+    using System.IO;
+    using System.Linq;
+
+    using HQC.Project.Hangman.Common;
+
+    public class FolderContentReader
+    {
+        private readonly string fileExtension = "*.TXT";
+        
+        public FolderContentReader()
+        {
+        }
+
+        public string[] getCategories()
+        {
+            return Directory.GetFiles(Globals.CategoriesPath, fileExtension)
+                .Select(path => Path.GetFileNameWithoutExtension(path))
+                .ToArray();
+        }
+    }
+}
