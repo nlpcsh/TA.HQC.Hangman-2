193870adbf46bd7c84598af12578a907e33ef3fe
ReniGetskova <reni.getskova@gmail.com>
2015-10-06 14:35:21 +0300

    Added interface IExporter.cs and class FileExporter.cs

---
+++ HQC.Project.Hangman-2/HQC.Project.Hangman2.1/Exporters/FileExporter.cs
@@ -0,0 +1,41 @@
+﻿using HQC.Project.Hangman.Common;
+using HQC.Project.Hangman.Importers;
+using HQC.Project.Hangman.Importers.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HQC.Project.Hangman2._1.Exporters
+{
+    public class FileExporter : IExporter
+    {
+        private ImportTopPlayersFromFile players;
+        public FileExporter(ImportTopPlayersFromFile players) 
+        {
+            this.players = players;
+        }
+        public void Save(string filename)
+        {
+
+            using (var writer = new StreamWriter(filename))
+            {
+                var sortedScores = this.players.TopPlayers.OrderBy(x => x.Score);
+
+                foreach (var score in sortedScores)
+                {
+                    if (score == null)
+                    {
+                        writer.WriteLine(Globals.FreePositionInScoreBoars);
+                    }
+                    else
+                    {
+                        writer.WriteLine(score.Name + "-" + score.Score);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ HQC.Project.Hangman-2/HQC.Project.Hangman2.1/Exporters/IExporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HQC.Project.Hangman2._1.Exporters
+{
+    public interface IExporter
+    {
+        void Save(string name);
+    }
+}
